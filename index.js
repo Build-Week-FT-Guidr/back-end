c54b417adf0f027206b906de454d95deecfcf3d0
Roger McConkie <roger.mcconkiejr@gmail.com>
2019-10-21 13:23:44 -0600

    nodemon added to package.json

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const server = require('./server');
 
-const PORT = process.env.PORT || 5656;
+const PORT = process.env.PORT || 5555;
 server.listen(PORT, () => {
   console.log(`\n=== Server listening on port ${PORT} ===\n`);
 });
