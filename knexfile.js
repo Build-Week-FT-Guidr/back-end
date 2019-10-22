31a44f23054b934a2655a113bf3ed2408d83e0ce
Trip1701 <bj2336@email.vccs.edu>
2019-10-22 11:44:03 -0400

    PG Config

--- knexfile.js
+++ knexfile.js
@@ -26,20 +26,19 @@ module.exports = {
   //   }
   // },
 
-  // production: {
-  //   client: 'postgresql',
-  //   connection: {
-  //     database: 'my_db',
-  //     user:     'username',
-  //     password: 'password'
-  //   },
-  //   pool: {
-  //     min: 2,
-  //     max: 10
-  //   },
-  //   migrations: {
-  //     tableName: 'knex_migrations'
-  //   }
-  // }
-
+  production: {
+    client: 'pg',
+    connection: {
+      database: 'my_db',
+      user:     'postgres',
+      password: 'postgres'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  }
 };
