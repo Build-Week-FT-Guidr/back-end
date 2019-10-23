c5a18c2fc2c0f020688bc7e711179e3d21898f3a
Trip4077 <bj2336@email.vccs.edu>
2019-10-23 13:46:57 -0400

    deployment configuration

--- knexfile.js
+++ knexfile.js
@@ -7,34 +7,25 @@ module.exports = {
       filename: "./data/guidrBuild.db3"
     },
     useNullAsDefault: true,
-    debug: true
+    debug: true,
+    migrations: {
+      directory: './migrations',
+    },
+    seeds: {
+      directory: './seeds'
+    },
   },
 
-  // staging: {
-  //   client: 'postgresql',
-  //   connection: {
-  //     database: 'my_db',
-  //     user:     'username',
-  //     password: 'password'
-  //   },
-  //   pool: {
-  //     min: 2,
-  //     max: 10
-  //   },
-  //   migrations: {
-  //     tableName: 'knex_migrations'
-  //   }
-  // },
 
   production: {
     client: 'pg',
     connection: process.env.DATABASE_URL,
     useNullAsDefault: true,
     migrations: {
-      directory: './data/migrations',
+      directory: './migrations',
     },
     seeds: {
-      directory: './data/seeds'
+      directory: './seeds'
     },
   },
 };
