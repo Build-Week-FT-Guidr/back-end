7cfbc116c9022b5cb2e8eafcbf41aab6cafe5c98
Bernard Johnson <bj2336@email.vccs.edu>
2019-10-23 18:53:57 -0400

    log replaced

--- users/users-router.js
+++ users/users-router.js
@@ -97,7 +97,7 @@ router.get('/', authenticated, (req, res) => {
         res.status(201).json(saved);
       })
       .catch(error => {
-        
+        console.log("err", err);
         res.status(500).json(error);
       });
   });
